f43faa3e2ab826b5ad870f9f31b65734a81cfbbc
Akinori MUSHA <knu@idaemons.org>
2016-06-28 19:47:00 +0900

    RssAgent: Migrate from FeedNormalizer to Feedjira
    FeedNormalizer is no longer maintained, and its Atom support has flaws
    in that it throws away what RSS::Parser returns and falls back to using
    SimpleRSS which is not capable of handling XML entities, resulting in
    getting ususable URLs such as ones including `&amp;`.
    
    Feedjira is highly customizable as it implements parsers for various
    feed formats on its own using sax-machine.
    
    The `clean` option is reimplemented using the loofah gem.
    `Feedjira::FeedUtilities#sanitize_entries!` is not used because it
    tries to sanitize non-HTML properties too.
    
    A new option `include_feed_info` is added.
    
    A new key `author` is added because `authors` only has one element.
    
    A new key `links` is added, which lists all `link` elements.

--- app/models/agents/rss_agent.rb
+++ app/models/agents/rss_agent.rb
@@ -1,5 +1,4 @@
 require 'rss'
-require 'feed-normalizer'
 
 module Agents
   class RssAgent < Agent
@@ -9,21 +8,23 @@ module Agents
     can_dry_run!
     default_schedule "every_1d"
 
+    gem_dependency_check { defined?(Feedjira::Feed) }
+
     DEFAULT_EVENTS_ORDER = [['{{date_published}}', 'time'], ['{{last_updated}}', 'time']]
 
     description do
       <<-MD
         The RSS Agent consumes RSS feeds and emits events when they change.
 
-        This Agent is fairly simple, using [feed-normalizer](https://github.com/aasmith/feed-normalizer) as a base.  For complex feeds
-        with additional field types, we recommend using a WebsiteAgent.  See [this example](https://github.com/cantino/huginn/wiki/Agent-configuration-examples#itunes-trailers).
+        For complex feeds with additional field types, we recommend using a WebsiteAgent.  See [this example](https://github.com/cantino/huginn/wiki/Agent-configuration-examples#itunes-trailers).
 
         If you want to *output* an RSS feed, use the DataOutputAgent.
 
         Options:
 
           * `url` - The URL of the RSS feed (an array of URLs can also be used; items with identical guids across feeds will be considered duplicates).
-          * `clean` - Attempt to use [feed-normalizer](https://github.com/aasmith/feed-normalizer)'s' `clean!` method to cleanup HTML in the feed.  Set to `true` to use.
+          * `include_feed_info` - Set to `true` to include feed information in each event.
+          * `clean` - Set to `true` to sanitize `description` and `content` as HTML fragments, removing unknown/unsafe elements and attributes.
           * `expected_update_period_in_days` - How often you expect this RSS feed to change.  If more than this amount of time passes without an update, the Agent will mark itself as not working.
           * `headers` - When present, it should be a hash of headers to send with the request.
           * `basic_auth` - Specify HTTP basic auth parameters: `"username:password"`, or `["username", "password"]`.
@@ -53,17 +54,41 @@ module Agents
       Events look like:
 
           {
+            "feed": {
+              "id": "...",
+              "type": "atom",
+              "generator": "...",
+              "url": "http://example.com/",
+              "links": [
+                { "href": "http://example.com/", "rel": "alternate", "type": "text/html" },
+                { "href": "http://example.com/index.atom", "rel": "self", "type": "application/atom+xml" }
+              ],
+              "title": "Some site title",
+              "description": "Some site description",
+              "copyright": "...",
+              "icon": "http://example.com/icon.png",
+              "author": "...",
+              "authors": [ "..." ],
+              "date_published": "Thu, 11 Sep 2014 01:30:00 -0700",
+              "last_updated": "Thu, 11 Sep 2014 01:30:00 -0700"
+            },
             "id": "829f845279611d7925146725317b868d",
-            "date_published": "2014-09-11 01:30:00 -0700",
-            "last_updated": "Thu, 11 Sep 2014 01:30:00 -0700",
             "url": "http://example.com/...",
             "urls": [ "http://example.com/..." ],
+            "links": [
+              { "href": "http://example.com/...", "rel": "alternate" },
+            ],
+            "title": "Some title",
             "description": "Some description",
             "content": "Some content",
-            "title": "Some title",
-            "authors": [ ... ],
-            "categories": [ ... ]
+            "author": "...",
+            "authors": [ "..." ],
+            "categories": [ "..." ],
+            "date_published": "2014-09-11 01:30:00 -0700",
+            "last_updated": "Thu, 11 Sep 2014 01:30:00 -0700"
           }
+
+      The `feed` key is present only if `include_feed_info` is true.
 
     MD
 
@@ -100,8 +125,7 @@ module Agents
         begin
           response = faraday.get(url)
           if response.success?
-            feed = FeedNormalizer::FeedNormalizer.parse(response.body, loose: true)
-            feed.clean! if boolify(interpolated['clean'])
+            feed = Feedjira::Feed.parse(response.body)
             new_events.concat feed_to_events(feed)
           else
             error "Failed to fetch #{url}: #{response.inspect}"
@@ -124,10 +148,6 @@ module Agents
       log "Fetched #{urls.to_sentence} and created #{created_event_count} event(s)."
     end
 
-    def get_entry_id(entry)
-      entry.id.presence || Digest::MD5.hexdigest(entry.content)
-    end
-
     def check_and_track(entry_id)
       memory['seen_ids'] ||= []
       if memory['seen_ids'].include?(entry_id)
@@ -139,21 +159,200 @@ module Agents
       end
     end
 
-    def feed_to_events(feed)
-      feed.entries.map { |entry|
-        Event.new(payload: {
-                    id: get_entry_id(entry),
-                    date_published: entry.date_published,
-                    last_updated: entry.last_updated,
-                    url: entry.url,
-                    urls: entry.urls,
-                    description: entry.description,
-                    content: entry.content,
-                    title: entry.title,
-                    authors: entry.authors,
-                    categories: entry.categories
-                  })
+    LINK_ATTRS = %i[href rel type hreflang title length]
+
+    unless dependencies_missing?
+      class AtomAuthor
+        include SAXMachine
+
+        element :name
+        element :email
+        element :uri
+      end
+
+      class AtomLink
+        include SAXMachine
+
+        LINK_ATTRS.each do |attr|
+          attribute attr
+        end
+
+        def to_json(options = nil)
+          LINK_ATTRS.each_with_object({}) { |key, hash|
+            if value = __send__(key)
+              hash[key] = value
+            end
+          }.to_json(options)
+        end
+      end
+
+      class RssLinkElement
+        include SAXMachine
+
+        value :href
+
+        def to_json(options = nil)
+          {
+            href: href
+          }.to_json(options)
+        end
+      end
+
+      module HasLinks
+        def self.included(mod)
+          mod.module_exec do
+            sax_config.top_level_elements['link'].clear
+            sax_config.collection_elements['link'].clear
+
+            case name
+            when /RSS/
+              elements :link, class: RssLinkElement, as: :rss_links
+
+              case name
+              when /FeedBurner/
+                elements :'atok10:link', class: AtomLink, as: :atom_links
+
+                def links
+                  @links ||= [*rss_links, *atom_links]
+                end
+              else
+                alias_method :links, :rss_links
+              end
+            else
+              elements :link, class: AtomLink, as: :links
+            end
+
+            def alternate_link
+              links.find { |link|
+                link.is_a?(AtomLink) &&
+                  link.rel == 'alternate' &&
+                  (link.type == 'text/html'|| link.type.nil?)
+              }
+            end
+
+            def url
+              @url ||= (alternate_link || links.first).try!(:href)
+            end
+          end
+        end
+      end
+
+      module FeedEntryExtensions
+        def self.included(mod)
+          mod.module_exec do
+            include HasLinks
+          end
+        end
+      end
+
+      module FeedExtensions
+        def self.included(mod)
+          mod.module_exec do
+            include HasLinks
+
+            element  :id, as: :feed_id
+            element  :generator
+            elements :rights
+            element  :published
+            element  :updated
+            element  :icon
+            elements :author, class: AtomAuthor, as: :authors
+
+            if /RSS/ === name
+              element :guid, as: :feed_id
+              element :managingEditor
+              element :pubDate, as: :published
+              element :'dc:date', as: :published
+              element :lastBuildDate, as: :updated
+              element :image, value: :url, as: :icon
+            end
+
+            sax_config.collection_elements.each_value do |collection_elements|
+              collection_elements.each do |collection_element|
+                collection_element.accessor == 'entries' &&
+                  (entry_class = collection_element.data_class).is_a?(Class) or next
+
+                entry_class.send :include, FeedEntryExtensions
+              end
+            end
+          end
+        end
+      end
+
+      Feedjira::Feed.feed_classes.each do |feed_class|
+        feed_class.send :include, FeedExtensions
+      end
+    end
+
+    def feed_data(feed)
+      type =
+        case feed.class.name
+        when /Atom/
+          'atom'
+        else
+          'rss'
+        end
+
+      authors = feed.authors.map(&:name).presence ||
+                Array(feed.try(:managingEditor) || feed.try(:itunes_author))
+
+      {
+        id: feed.feed_id,
+        type: type,
+        url: feed.url,
+        links: feed.links,
+        title: feed.title,
+        description: feed.description,
+        copyright: feed.try(:copyright) || feed.rights.join("\n").presence,
+        generator: feed.generator,
+        icon: feed.icon,
+        authors: authors,
+        date_published: feed.published,
+        last_updated: feed.updated || feed.published,
       }
+    end
+
+    def entry_data(entry)
+      description = entry.summary
+      content = entry.content || description
+      id = entry.try(:entry_id) || Digest::MD5.hexdigest(content || '')
+      author = entry.author || entry.try(:itunes_author)
+
+      {
+        id: id,
+        url: entry.url,
+        urls: entry.links.map(&:href),
+        links: entry.links,
+        title: entry.title,
+        description: clean_fragment(description),
+        content: clean_fragment(content),
+        image: entry.try(:image),
+        author: author,
+        authors: Array(author),
+        categories: Array(entry.try(:categories)),
+        date_published: entry.published,
+        last_updated: entry.try(:updated) || entry.published,
+      }
+    end
+
+    def feed_to_events(feed)
+      payload_base = {}
+
+      if boolify(interpolated['include_feed_info'])
+        payload_base[:feed] = feed_data(feed)
+      end
+
+      feed.entries.map { |entry|
+        Event.new(payload: payload_base.merge(entry_data(entry)))
+      }
+    end
+
+    def clean_fragment(fragment)
+      if boolify(interpolated['clean']) && fragment.present?
+        Loofah.scrub_fragment(fragment, :prune).to_s
+      else
+        fragment
+      end
     end
   end
 end
